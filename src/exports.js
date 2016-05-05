5749d2a05e9e12f722d27e4d2c6a5f1941bc4f93
Cameron Jensen <cameron@ionata.com.au>
2016-05-05 13:26:59 +1000

    working example

---
+++ src/exports.js
@@ -0,0 +1,3 @@
+import Slider from './slider'
+
+window.Slider = window.Slider || Slider
--- src/slider.js
+++ src/slider.js
@@ -1,54 +1,77 @@
 class Slider {
   static defaults = {
-    transitionDuration: 1000,   // slide duration time
-    transitionDelay: 8000,      // slide delay
+    transitionDelay: 4000,      // slide delay
     buttons: true,
-    items: '.slider-item'
+    items: '.slider-item'       // slider items selector
   }
   
   timer = null     // the timeout object
   active = 0       // the active slide (zero-indexed), default to 0
   
+  /**
+   * @param {String} selector
+   * @param {Object} args
+   */
   constructor (selector = '', args = {}) {
     this.args = Object.assign(Slider.defaults, args)
     this.el = document.querySelector(selector)
     this.items = Array.from(this.el.querySelectorAll(args.items))
     
-    if (items.length > 1 && this.args.buttons) {
+    console.log(this.items)
+    
+    // render prev and next buttons
+    if (this.items.length > 1 && this.args.buttons) {
       this.createButtons()
     }    
     // start
+    this.setActive()
     this.queue()
   }
   
+  /**
+   * Create the `prev` and `next` buttons
+   */
   createButtons () {
     // prev
     const prevButton = document.createElement('button')
     prevButton.type = 'button'    
-    prevButton.addEventListener('click', this.prev)    
+    prevButton.addEventListener('click', ::this.prev)    
     this.el.appendChild(prevButton)
     
     // next
     const nextButton = document.createElement('button')
     nextButton.type = 'button'    
-    nextButton.addEventListener('click', this.prev)
+    nextButton.addEventListener('click', ::this.next)
     this.el.appendChild(nextButton)
   }
   
+  /**
+   * Set the active data attr 
+   */
   setActive () {
     this.el.dataset.active = this.active
   }
   
+  /**
+   * Queue next
+   */
   queue () {
-    this.timer = setTimeout(this.next, this.args.transitionDelay)
+    clearTimeout(this.timer)
+    this.timer = setTimeout(::this.next, this.args.transitionDelay)
   }
   
+  /**
+   * Forward a slide
+   */
   next () {
-    clearTimeout(this.timer)
     this.active = this.active+1 < this.items.length ? this.active+1 : 0
     this.setActive()
+    this.queue()
   }
   
+  /**
+   * Back a slide
+   */  
   prev () {
     clearTimeout(this.timer)
     this.active = this.active-1 >= 0 ? this.active - 1 : this.items.length
